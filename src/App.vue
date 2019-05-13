3830761fec7b5483d5cce8806935ad6133321364
Guimaraes Mahota <gmahota@MERIDIAN.CO.MZ>
2019-05-13 09:43:11 +0200

    axios managment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <!-- navbar -->
+      <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
         <div class="container">
         <router-link to="/" class="navbar-brand">Home</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,10 +23,21 @@
         </div>
         </div>
       </div>
-    <router-view/>
+      <div class="container">
+       <transition name="moveInUp">
+         <router-view/>
+       </transition>
+        
+      </div>
+    
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
